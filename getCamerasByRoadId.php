b5cb804cee4a50b0ced207aa7ad21bc25cceccd9
chap19150 <kchapman@Kennys-MacBook-Pro.local>
2013-09-21 13:46:04 -0400

    endpoint to get cameras by roadid

---
+++ getCamerasByRoadId.php
@@ -0,0 +1,55 @@
+
+<?php
+/* require the user as the parameter */
+if(isset($_GET['roadid']) && strval($_GET['roadid'])) {
+
+	/* soak in the passed variable or set our own */
+	$roadid = strval($_GET['roadid']); //no default
+        $format = isset($_GET['format']) ? strval($_GET['format']) : 'json';
+
+	/* connect to the db */
+	$link = mysql_connect('url','username','password') or die('Cannot connect to the DB');
+	mysql_select_db('penndot_cameras',$link) or die('Cannot select the DB');
+	
+		$query = "SELECT * FROM cameras where cameras.road_id = '$roadid'";
+	
+	/* grab the cameras from the db */
+	
+	
+	$result = mysql_query($query,$link) or die('Errant query:  '.$query);
+	/* create one master array of the records */
+	$cameras = array();
+	if(mysql_num_rows($result)) {
+		while($camera = mysql_fetch_assoc($result)) {
+			$cameras[] = array('camera'=>$camera);
+		}
+	}
+
+	/* output in necessary format */
+	if($format == 'json') {
+		header('Content-type: application/json');
+		echo json_encode(array('cameras'=>$cameras));
+	}
+	else {
+		header('Content-type: text/xml');
+		echo '<cameras>';
+		foreach($cameras as $index => $camera) {
+			if(is_array($camera)) {
+				foreach($camera as $key => $value) {
+					echo '<',$key,'>';
+					if(is_array($value)) {
+						foreach($value as $tag => $val) {
+							echo '<',$tag,'>',htmlentities($val),'</',$tag,'>';
+						}
+					}
+					echo '</',$key,'>';
+				}
+			}
+		}
+		echo '</cameras>';
+	}
+
+	/* disconnect from the db */
+	@mysql_close($link);
+}
+?>
